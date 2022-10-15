84cb602e016cb573f314bac41b0ef9ebf23f8775
Alexandr <a.semashkev14@gmail.com>
2022-10-15 19:39:40 +0300

    Решение

--- Task/Program.cs
+++ Task/Program.cs
@@ -11,3 +11,11 @@ string[] arrayA = {"123", "1", "hello", "world", "=)"};
 string[] arrayB = new string[arrayA.Length];
 int count = 0;
 
+for (int i = 0; i < arrayA.Length; i++)
+{
+    if(arrayA[i].Length <= 3)
+    {
+        arrayB[count] = arrayA[i];
+        count++;
+    }
+}
